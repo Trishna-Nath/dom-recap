41cf199e937da7af3c13842c51e439ce1b0cca69
Trishna-Nath <t.nathbd@gmail.com>
2023-02-19 22:15:24 -0800

    added style

--- comment.js
+++ comment.js
@@ -1,3 +1,4 @@
+//milestone-5
 // 1 - how to use js with html and css 
 // 2 get element or elements from dom
 
@@ -20,3 +21,28 @@
 // 6.use function to reduce duplicate ConvolverNode7. 
 
 //7 event bubble and event deligate
+
+
+//start milestone-6
+
+ document.getElementById('apply-bg').addEventListener('click',function(){
+     const friends = document.getElementsByClassName('friend');
+     for(const friend of friends){
+        friend.style.backgroundColor = 'lightblue';
+     }
+ });
+ document.getElementById('center').addEventListener('click',function(){
+    const centeringThirdOne = document.getElementById('third');
+    centeringThirdOne.style.textAlign = 'center';
+ });
+
+ document.getElementById('add-new').addEventListener('click',function(){
+    const friendContainer = document.getElementById('friends');
+    const friend = document.createElement('div');
+    friend.classList.add('friend');
+    friend.innerHTML = `
+    <h3 class = "friend-name">new friend</h3>
+    <p>something new added </p>
+    `
+    friendContainer.appendChild(friend);
+ })
